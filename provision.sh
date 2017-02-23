46d62586859bbf12369f2fd368de7f3614033533
Paweł Barański <p.baranski@clearcode.cc>
2017-02-24 00:05:49 +0100

    provision

--- provision.sh
+++ provision.sh
@@ -2,15 +2,14 @@
 
 add-apt-repository ppa:ondrej/php
 apt-get update
-apt-get install -y git nginx
-
-debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
-apt-get install -y mysql-server
+apt-get install -y git
 
 apt-get install -y php7.1-fpm \
                    php7.1-cli \
-                   php7.1-mysql \
                    php7.1-curl \
                    php7.1-intl \
+                   php7.1-xml \
                    php7.1-sqlite3
+
+echo "export PS1='\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[0;33m\]\w\[\033[m\]\$ '" >> /home/ubuntu/.bashrc
+echo "export LS_COLORS='di=0;32:ln=0;36'" >> /home/ubuntu/.bashrc
