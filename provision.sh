dc607b5693fbe609848f7b1a844cd98cbc0ffd16
Paweł Barański <p.baranski@clearcode.cc>
2017-02-26 12:37:36 +0100

    xdebug added

--- provision.sh
+++ provision.sh
@@ -9,7 +9,8 @@ apt-get install -y php7.1-fpm \
                    php7.1-curl \
                    php7.1-intl \
                    php7.1-xml \
-                   php7.1-sqlite3
+                   php7.1-sqlite3 \
+                   php-xdebug
 
 echo "export PS1='\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[0;33m\]\w\[\033[m\]\$ '" >> /home/ubuntu/.bashrc
 echo "export LS_COLORS='di=0;32:ln=0;36'" >> /home/ubuntu/.bashrc
