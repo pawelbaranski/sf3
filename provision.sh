45b314465a183da3d8007c04f600b7fb0f1a6c5d
Paweł Barański <p.baranski@clearcode.cc>
2017-03-24 20:21:24 +0100

    zip php

--- provision.sh
+++ provision.sh
@@ -8,6 +8,7 @@ apt-get install -y php7.1-cli \
                    php7.1-curl \
                    php7.1-intl \
                    php7.1-xml \
+                   php7.1-zip \
                    php7.1-sqlite3 \
                    php-xdebug
 
